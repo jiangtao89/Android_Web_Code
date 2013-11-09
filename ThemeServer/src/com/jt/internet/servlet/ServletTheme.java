54563737e2e6d4ae5214633519c596dff976b840
jtaizlx <jtao0102@gmail.com>
2013-11-09 16:20:54 +0800

    ThemeServer

---
+++ ThemeServer/src/com/jt/internet/servlet/ServletTheme.java
@@ -0,0 +1,47 @@
+package com.jt.internet.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletTheme
+ */
+@WebServlet("/ServletTheme")
+public class ServletTheme extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletTheme() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name = request.getParameter("name");
+		String age = request.getParameter("age");
+		System.out.println("doGet name= " + name);
+		System.out.println("doGet age= " + age);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name = request.getParameter("name");
+		String age = request.getParameter("age");
+		System.out.println("doPost name= " + name);
+		System.out.println("doPost age= " + age);
+	}
+
+}
